94b8e3da667181504e8f307f2bc17bf91b4bfd73
Leenix <leenix48@gmail.com>
2015-06-28 00:45:44 +1000

    Shebangs

---
+++ MotionNotification.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import socket
+import datetime
+
+SERVER_ADDRESS = 'localhost'
+SERVER_PORT = 8888
+
+# Connect to the server
+client_socket = socket.socket()
+client_socket.connect((SERVER_ADDRESS, SERVER_PORT))
+
+# Send the payload
+payload = {'id': 'motionNotification', 'timestamp': datetime.datetime.now().isoformat()}
+client_socket.send(str(payload) + '\n')
+client_socket.close()
---
+++ PlayRandomMusicFile.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import pyglet
+import os
+import random
+import re
+
+DETECTION_NOISES_PATH = "\detection_start\\"
+DETECTION_END_NOISES_PATH = "\detection_end\\"
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+def play_random_file(file_path):
+    file_list = build_file_list(file_path)
+    random.shuffle(file_list)
+    play_song(file_list[0])
+
+
+def build_file_list(file_path):
+    file_list = []
+    for root, folders, files in os.walk(file_path):
+        folders.sort()
+        files.sort()
+        for filename in files:
+            if re.search(".(aac|mp3|wav|flac|m4a|pls|m3u)$", filename) is not None:
+                file_list.append(os.path.join(root, filename))
+    return file_list
+
+
+def play_song(file):
+    print "Playing sound: {}".format(file)
+    song = pyglet.media.load(file)
+    song.play()
+    pyglet.clock.schedule_once(exiter, song.duration)
+    pyglet.app.run()
+
+
+def exiter(dt):
+    pyglet.app.exit()
+
+if __name__ == '__main__':
+    play_random_file(path + DETECTION_NOISES_PATH)
